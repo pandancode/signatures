fc05e93e6556a2a99ea6ad18f34b91e9000036be
Amin Mallol <aminm-34@hotmail.fr>
2021-11-22 22:21:41 +0000

    Created controllers contracts and signatories. Need to double check in group tomorrow. Also addedd the route contracts/company

--- app/controllers/signatories_controller.rb
+++ app/controllers/signatories_controller.rb
@@ -1,2 +1,43 @@
 class SignatoriesController < ApplicationController
+  def show
+    @signatory = Signatory.find(signatory_params)
+  end
+
+  def new
+    @contract = Contract.find(params[:id])
+    # TO CHECK BELOW
+    @individual = Individual.find(params[:id])
+    @signatory = Signatory.new
+  end
+
+  def create
+    # Double check below
+    @signatory = Signatory.new
+    @contract = Contract.find(params[:contract_id])
+    @individual = Individual.find(params[:individual_id])
+    @signatory.contract = @contract
+    @signaory.individual = @individual
+    if @signatory.save
+      redirect_to contract_path(@contract)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @signatory = Signatory.find(params[:id])
+  end
+
+  def update
+    @signatory = Signatory.find(params[:id])
+    @signatory.update(signatory_params)
+    # Double check below
+    redirect_to signatory_path(@signatory)
+  end
+
+  private
+
+  def signatory_params
+    params.require(:signatory).permit(:signing_status)
+  end
 end
