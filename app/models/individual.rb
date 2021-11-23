c21aedf2fc72a449c3f5ac13a55ddd155bd44c87
Amin Mallol <67249378+Focus-me34@users.noreply.github.com>
2021-11-23 12:58:15 +0000

    Updated
    Updated the order of the validations so it fits with the schema

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -4,20 +4,31 @@ class Individual < ApplicationRecord
   has_many :addresses
   has_many :users
 
-  validates :title, presence: true
   validates :first_name, presence: true
+  validates :first_name, numericality: { only_integer: false }
+  
+  validates :last_name, numericality: { only_integer: false }
   validates :last_name, presence: true
+  
   validates :birth_date, presence: true
+  
   validates :phone_number, presence: true
-  #length 10 to 11 numbers for phone number
   validates :phone_number, length: { minimum: 10 }
   validates :phone_number, length: { maximum: 11 }
-  #only number for phone number  
   validates :phone_number, numericality: { only_integer: true }
-  #title validation for mr mrs
+  
+  validates :title, presence: true
   validates :title, inclusion: { in: %w(Mr Ms)} 
-  #only charachter for title name and last name
   validates :title, numericality: { only_integer: false }
-  validates :first_name, numericality: { only_integer: false } 
-  validates :last_name, numericality: { only_integer: false } 
+  
+
+  
+  
+  
+
+  
+
+  
+
+
 end
