e95d266973708ae7f8b12babf950b452db7a0757
Sarah McElligott <sarahmce@protonmail.com>
2021-11-23 12:20:28 +0000

    migration file for company manually created and models for company and user created and validations and associations entered

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :companies
+  has_many :individual
+
+  validates :role, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
