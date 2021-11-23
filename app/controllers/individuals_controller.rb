58913cfc71bc6ed78ce214612b8dd302eb036ab8
Sarah McElligott <sarahmce@protonmail.com>
2021-11-23 16:59:13 +0000

    individuals controller

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -1,2 +1,17 @@
 class IndividualsController < ApplicationController
+  def show
+    @individual = Individual.find(individual_params)
+  end
+
+  def update
+    @individual = Individual.find(params[:id])
+    @individual.update(individual_params)
+    redirect_to individual_path(@individual)
+  end
+
+private
+
+  def individual_params
+    params.require(:individual).permit(:fully_signed_at)
+  end
 end
