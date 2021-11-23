065eb2972b7757f6e2fea45ef3a244a529deecee
Sarah McElligott <sarahmce@protonmail.com>
2021-11-23 17:32:36 +0000

    completed individuals controller - really not sure about this, particularly create method so please check

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -1,4 +1,18 @@
 class IndividualsController < ApplicationController
+  def new
+    @individual = Individual.new
+  end
+
+  def create
+    @individual = Individual.new(individual_params)
+    @user = current_user
+    if @individual.save
+      redirect_to individual_path(@individual)
+    else
+      render :new
+    end
+  end
+
   def show
     @individual = Individual.find(individual_params)
   end
@@ -6,7 +20,6 @@ class IndividualsController < ApplicationController
   def update
     @individual = Individual.find(params[:id])
     @individual.update(individual_params)
-    redirect_to individual_path(@individual)
   end
 
 private
