0e086a7f035496180be554b6e0c8dc3c97e48b9c
Sarah McElligott <sarahmce@protonmail.com>
2021-11-24 10:40:19 +0000

    minor addition to routes and edit definition added to individuals controller - work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :individuals, only: %i[new create show update] do
+  resources :individuals, only: %i[new create show edit update] do
     resources :signatories, only: %i[show edit update]
   end
   resources :contracts, only: %i[index new create show] do
