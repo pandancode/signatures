76b538550a60d776316e2b1199d15c88930de138
Yilun Chen <yilun.chen@web.de>
2021-11-25 00:48:42 +0000

    This is the most complete version, about to roll back all work on the Contracts Controller#AllActions + associated Contract Views

---
+++ config/initializers/aws.rb
@@ -0,0 +1,7 @@
+Aws.config[:credentials] = Aws::Credentials.new(
+  ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
+  ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+)
+Aws.config[:region]      = 'eu-west-1'
+
+S3 = Aws::S3::Client.new
