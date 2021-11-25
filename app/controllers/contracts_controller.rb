cd3c8fc1ddffd380af50e7e9880fd801738c6112
Giorgio Gristina <giorgio.gristina91@gmail.com>
2021-11-25 16:56:26 +0000

    implemented

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -3,14 +3,12 @@ class ContractsController < ApplicationController
     #if the current user role is a company
     if current_user.role == "Company"
       #display all the contract where company_id 
-      # @company = current_user.company
-      # @contracts = @company.contracts  current_user.company
+      
       @contracts = Contract.where(company_id: current_user.company)
       #else 
     else
       #display all contract where individual_id
-      # @individual = current_user.individual
-      # @contracts = @individual.contracts   
+     
       @contracts = Contract.where(individual_id: current_user.individual)      
     end    
   end
@@ -21,28 +19,37 @@ class ContractsController < ApplicationController
 
   def new
     #if u are a company
-    @contract = Contract.new
+    if current_user.role == "Company"
+      @contract = Contract.new
+    else
+      redirect_to contracts_path
+      flash.alert = "You are not allow to create a contract"
+    end
   end
 
   def create
-    #get company id
-    @company = current_user.company.id
-    
-    @contract = Contract.new(contract_params)
-    #set the company id
-    @contract.company_id = @company
-    #find user by recipiant email
-    @user = User.find_by(email: @contract.recipient_email)
-    # git the individual id
-    @individual = Individual.find_by(user_id: @user).id
-    #assign to contract individual_id
-    @contract.individual_id = @individual
-    if @contract.save
-      redirect_to contract_path(@contract)
+    if current_user.role == "Company"
+      #get company id
+      @company = current_user.company.id
+      
+      @contract = Contract.new(contract_params)
+      #set the company id
+      @contract.company_id = @company
+      #find user by recipiant email
+      @user = User.find_by(email: @contract.recipient_email)
+      # git the individual id
+      @individual = Individual.find_by(user_id: @user).id
+      #assign to contract individual_id
+      @contract.individual_id = @individual
+      if @contract.save
+        redirect_to contract_path(@contract)
+      else
+        render :new
+      end
     else
-      render :new
+      redirect_to contracts_path
+      flash.alert = "You are not allow to create a contract"
     end
-    
   end
 
   private
