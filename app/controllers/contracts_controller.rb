9364b00eda33a3791d7d3ddd7043344d743846c6
Giorgio Gristina <giorgio.gristina91@gmail.com>
2021-11-25 12:11:01 +0000

    index done

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -3,14 +3,14 @@ class ContractsController < ApplicationController
     #if the current user role is a company
     if current_user.role == "Company"
       #display all the contract where company_id 
-      @company = current_user.company
-      @contracts = @company.contracts    
+      # @company = current_user.company
+      # @contracts = @company.contracts  current_user.company
+      @contracts = Contract.where(company_id: current_user.company)
       #else 
     else
       #display all contract where individual_id
       @individual = current_user.individual
-      @contracts = @individual.contracts    
-      
+      @contracts = @individual.contracts         
     end    
   end
 
