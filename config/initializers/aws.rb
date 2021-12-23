dfbc73d416dfe67a5a603386f85bb519a30d7402
Amin Mallol <aminm-34@hotmail.fr>
2021-12-23 18:46:16 +0000

    added region in env file and repalced value of Aws.config in aws.rb file with global variable

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,7 @@
 Aws.config[:credentials] = Aws::Credentials.new(
   ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
   ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY'],
-  s3_region: ENV['AWS_REGION']
 )
-Aws.config[:region] = 'eu-west-1'
+Aws.config[:region] = ENV['AWS_REGION']
 
 S3 = Aws::S3::Client.new
