2c0a59f3daa42ecbe5199feffb3b0695d967af07
Amin Mallol <aminm-34@hotmail.fr>
2021-12-23 18:34:09 +0000

    fixed space in aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,6 +2,6 @@ Aws.config[:credentials] = Aws::Credentials.new(
   ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
   ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
 )
-Aws.config[:region]      = 'eu-west-1'
+Aws.config[:region] = 'eu-west-1'
 
 S3 = Aws::S3::Client.new
