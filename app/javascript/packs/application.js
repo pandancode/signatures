f83e4f1867f82ab39f50fb647c9f9ebd5eb1f2dd
Amin Mallol <aminm-34@hotmail.fr>
2021-12-23 16:18:35 +0000

    rollbacked to 11th of Desecember version with app folder updated with responsive design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -38,5 +40,37 @@ document.addEventListener('turbolinks:load', () => {
   loader();
 });
 
+///////////////////// ALERT POPUPS WINDOWS (YILUN)
+
 $(".alert").fadeOut(4000);
 import "controllers"
+
+//////////////////// ADDING / REMOVING ACTION ICON FROM NAVBAR (AMIN) /////////////////////
+
+// INITIALISING THE VARIABLEs WHICH WILL STORE THE HTML CODE OF THE ACTION NAV ITEM AND NAVBAR ELEMENT
+const li_icon = document.querySelector(".li-icon");
+const li_iconCopy = document.querySelector(".li-icon");
+const navbarUlElem = document.querySelector(".navbar-nav");
+
+// FUNCTION THAT WILL ADD / REMOVE THE ACTION BUTTON DEPENDING ON THE "clientWidth" SIZE.
+
+const displayIconNavItem = () => {
+  if (document.documentElement.clientWidth <= 565) {
+    // REMOVES THE ACTION ICON NAV ITEM ELEMENT FROM DOM TREE
+    li_icon.parentNode.removeChild(li_icon);
+  } else {
+    // ADD THE ACTION ICON NAV ITEM ELEMENT TO DOM TREE
+    // document.querySelector(".li-icon") ? "" : navbarUlElem.insertBefore(li_icon, navbarUlElem.children[1]);
+    navbarUlElem.insertBefore(li_icon, navbarUlElem.children[1]);
+  }
+}
+
+// WHEN THE APP LOAD, CHECK IF SIZE <= 565 px.
+// IF YES, REMOVES THE ACTION ICON FROM THE DROPDOWN LIST IN THE NAVBAR.
+// IF NOT, JUST STORES THE HTML CODE OF THE ACTION BUTTON (SO HOME FOR INDIV / CONTRACT FOR COMPANIES)
+
+document.addEventListener("DOMContentLoaded", displayIconNavItem());
+// document.addEventListener("beforeunload", displayIconNavItem());
+
+// EVENT LISTENER CHECKING THE SIZE OF THE CLENT WIDTH AND CALLING "displayIconNavItem" EVERYTIME WINDOW IS RESIZED
+window.onresize = displayIconNavItem
